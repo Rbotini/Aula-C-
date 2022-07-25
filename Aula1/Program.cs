e315bf98b6ba52e89411a48a001736f1735c5ccb
= <=>
2022-07-25 18:07:47 -0300

    Criado metodo cadastrar

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -11,13 +11,24 @@ namespace Aula1
         static void Main(string[] args)
         {
             Console.WriteLine("Sistema Cadastro UBS");
+
+            List<string> cadastro = new List<string>();
             int opc = 0;
             while(opc != 3)
             {
                 Console.WriteLine("1-Cadastrar");
                 Console.WriteLine("2-Listar");
                 Console.WriteLine("3- Sair");
-                opc = int.Parse(Console.ReadLine());    
+                opc = int.Parse(Console.ReadLine());   
+                
+                if(opc == 1)
+                {
+                    Console.WriteLine("Digite o Nome");
+                    String nome;
+                    nome = Console.ReadLine();
+                    cadastro.Add(nome);
+
+                }
             }
         }
     }
