94dca869da3a6473f0e74d934345bc12d9feb31f
= <=>
2022-07-25 18:20:14 -0300

    Criado metodo remover

--- Aula1/Program.cs
+++ Aula1/Program.cs
@@ -14,11 +14,12 @@ namespace Aula1
 
             List<string> cadastro = new List<string>();
             int opc = 0;
-            while(opc != 3)
+            while(opc != 4)
             {
                 Console.WriteLine("1-Cadastrar");
                 Console.WriteLine("2-Listar");
-                Console.WriteLine("3- Sair");
+                Console.WriteLine("3-Remover");
+                Console.WriteLine("4-Sair");
                 opc = int.Parse(Console.ReadLine());   
                 
                 if(opc == 1)
@@ -28,13 +29,23 @@ namespace Aula1
                     nome = Console.ReadLine();
                     cadastro.Add(nome);
 
-                }else if(opc == 2)
+                  }else if(opc == 2)
                 {
                     Console.WriteLine("Lista de Pacientes");
                     foreach (String cad in cadastro)
                     {
                         
                         Console.WriteLine(cad);
+                    }
+                } else if(opc == 3)
+                {
+                    if(cadastro.Count > 0)
+                    {
+                        cadastro.RemoveAt(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lista Vazia");
                     }
                 }
             }
